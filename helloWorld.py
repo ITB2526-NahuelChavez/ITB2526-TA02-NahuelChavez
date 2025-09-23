134b9811ae730b2a57f9adff81e9901e3d55123d
nahuel <nahuel.chavez.7e6@itb.cat>
2025-09-23 12:52:05 +0200

    prueba3

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,11 @@
-print("Hola món soc Nahuel ;-) ")
-
+"""
+Nahuel Lautaro Chavez
+23/09/2025
+ASIXcA
+Descripció: Exemple dels apunts
+"""
+# Programa major d'edat.
+edat=int(input("Quina edat tens? "))
+if edat>=18:
+    print("Ets major d'edat")
+print("Programa Finalitzat")
